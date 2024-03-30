f1098710c6567a9803c046d855d654b98b28d1d6
xin li <40687850+2635410120@users.noreply.github.com>
2024-03-30 19:11:27 +0800

    1

---
+++ 期待搁浅.cpp
@@ -0,0 +1,13 @@
+bool she_smiles_for_me() {
+    //heart:
+    while (true) {
+        std::cout << "beat." << std::endl;
+        try {
+            std::this_thread::sleep_for(std::chrono::hours(24));
+        }
+        catch (const std::exception& e) {
+            // Catch any exceptions
+        }
+    }
+    return true;
+}////ÎÒÓÀÔ¶ÔÚÆÚ´ý¸éÇ³
---
+++ 未竟之爱.cpp
@@ -0,0 +1,6 @@
+class love {};
+void main()
+{
+    throw love();
+
+}//遗憾或许意味着残缺，但残缺也是另一种完整
